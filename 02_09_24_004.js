1e6bb59b49e2c1adb399a2eda2d5f81513aca389
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-09 15:47:32 -0500

    Update and rename 02_09_24_003.js to 02_09_24_004.js

--- 02_09_24_004.js
+++ 02_09_24_004.js
@@ -133,7 +133,7 @@ function AddSchoolFunc() {
     var span2 = document.createElement('span');
     span2.setAttribute("id", "newspan2");
     span2.setAttribute("class", "tooltip");
-    span2.setAttribute("style", "right:100%; bottom:100%; display:none");
+    span2.setAttribute("style", "right:50%; bottom:50%; display:none");
     list.appendChild(span2);
     var span3 = document.createElement('span');
     span3.setAttribute("id", "newspan3");

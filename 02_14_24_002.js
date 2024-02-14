328c4b7512dbe5d6b08263133f668109935ed9ae
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-14 09:55:45 -0500

    Update and rename 02_14_24_001.js to 02_14_24_002.js

--- 02_14_24_002.js
+++ 02_14_24_002.js
@@ -134,7 +134,7 @@ function AddSchoolFunc() {
     var span2 = document.createElement('span');
     span2.setAttribute("id", "newspan2");
     span2.setAttribute("class", "tooltip");
-    span2.setAttribute("style", "display:inline; left:50%");
+    span2.setAttribute("style", "left:50%");
     span.insertBefore(span2, span.children[0]);
     var span3 = document.createElement('span');
     span3.setAttribute("id", "newspan3");

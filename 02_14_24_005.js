e0311331fa6ffe7d2d7c5db7ccac5df7ea0c8429
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-14 10:01:04 -0500

    Update and rename 02_14_24_004.js to 02_14_24_005.js

--- 02_14_24_005.js
+++ 02_14_24_005.js
@@ -134,12 +134,11 @@ function AddSchoolFunc() {
     var span2 = document.createElement('span');
     span2.setAttribute("id", "newspan2");
     span2.setAttribute("class", "tooltip");
-    span2.setAttribute("style", "display:none; left:1%");
+    span2.setAttribute("style", "display:none; position:absolute; left:1%");
     span.insertBefore(span2, span.children[0]);
     var span3 = document.createElement('span');
     span3.setAttribute("id", "newspan3");
     span3.setAttribute("class", "tooltip-inner");
-    span3.setAttribute("style", "position:absolute");
     span3.innerText = "Add School Contact";
     document.getElementById("newspan2").appendChild(span3);
     document.getElementById("newbtn1").addEventListener('click', myClick);

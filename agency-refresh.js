0ea61586da1102269e92a3e2336ec03d2705929b
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-22 10:31:24 -0500

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -1,7 +1,7 @@
 var head= document.getElementsByTagName('head')[0];
 var script= document.createElement('script');
 var id = Math.random().toString(36).slice(2, 7);
-var src = "https://drewderose.github.io/jostenscustomcode/kairoscloud.js?" + id;
-script.setAttribute("id", "jostens-custom-code_kairos-cloud");
+var src = "https://drewderose.github.io/jostenscustomcode/agency-content.js?" + id;
+script.setAttribute("id", "jostens-custom-code_agency-content");
 script.src= src;
 head.appendChild(script);

fb1f9821c7ae58814fabf1d61e3e1e19148047a2
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-22 09:19:53 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 var head= document.getElementsByTagName('head')[0];
 var script= document.createElement('script');
 var id = Math.random().toString(36).slice(2, 7);
-var src = "https://drewderose.github.io/jostenscustomcode/main.js?" + id;
-script.setAttribute("id", "jostenscustomcode_home");
+var src = "https://drewderose.github.io/jostenscustomcode/kairoscloud.js?" + id;
+script.setAttribute("id", "jostens-custom-code_kairos-cloud");
 script.src= src;
 head.appendChild(script);

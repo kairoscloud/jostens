8e2f414c874af0c0375ddc92d03781b835417e89
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-22 14:36:43 -0500

    Update add-school-contact.js

--- add-school-contact.js
+++ add-school-contact.js
@@ -37,7 +37,7 @@ function intFunc() {
     // User Phone
     window.userphone = getPageParameter('p');
     if (userphone === "" || userphone === undefined) {
-      alert("Phone number missing.");
+      alert("ERROR CODE 201: User phone number missing. You will not be notified about the status of this submission.");
     } else {
       document.getElementById("rty6ckbPMktzvsYSAW1M").value = userphone;
       document.getElementById("rty6ckbPMktzvsYSAW1M").dispatchEvent(new Event("input", {
@@ -49,13 +49,16 @@ function intFunc() {
     document.getElementById('clearbutton1').addEventListener('click', clearFunction1);
     document.getElementById('mybutton2').addEventListener('click', MyFunc2);
     // Encoded URL
-    var mascot =
-    var school =
-    var location =
-    var logo =
+    var mascot = document.getElementById("DzcUKIO0kLtT5Xv5SP2D").value;
+    var school = document.getElementById("zCxe90EEKqnbK7aqJp4n").value;
+    var location = document.getElementById("US1EIuubo877vgGIwNQU").value;
+    var logo = document.getElementById("nxxune9mCfHvnxhZl3dO").value;
     var id = document.getElementById("4VnDd3UjXSNaugruH3k2").value;
     var phone = document.getElementById("rty6ckbPMktzvsYSAW1M").value;
-    document.getElementById("kNki5hB6i5ZvqlBEWjeT").value = 
+    var url = "ma=" + mascot + "&hs=" + school + "&cs=" + location + "&ml=" + logo + "&id=" + id + "&up=" + phone;
+    var url1 = encodeURIComponent(url);
+    var url2 = encodeURIComponent(url1);
+    document.getElementById("kNki5hB6i5ZvqlBEWjeT").value = url2;
   }
 }
 

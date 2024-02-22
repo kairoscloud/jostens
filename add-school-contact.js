866cd8b6bd194fbb660f6b76e1259c86420287e1
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-22 10:20:00 -0500

    Create add-school-contact.js

---
+++ add-school-contact.js
@@ -0,0 +1,93 @@
+// Page Parameters
+var getPageParameter = function getPageParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true :
+        decodeURIComponent(sParameterName[1]);
+    }
+  }
+}
+
+// Client Location ID
+window.locationid = getPageParameter('id');
+if (locationid === "" || locationid === undefined) {
+  alert("ERROR CODE 301: Please contact support.");
+  document.getElementById('mediaslink').href = "javascript:void(0);";
+} else {
+  var startUrl = "https://app.kairoscloud.io/location/";
+  var endUrl = "/medias";
+  var mediasUrl = startUrl + locationid + endUrl;
+  document.getElementById('mediaslink').href = mediasUrl;
+  document.getElementById("4VnDd3UjXSNaugruH3k2").value = locationid;
+  document.getElementById("4VnDd3UjXSNaugruH3k2").dispatchEvent(new Event("input", {
+    bubbles: true
+  }));
+}
+
+// Paste Logo Button
+async function pastebtn() {
+  try {
+    var text = await navigator.clipboard.readText();
+    document.getElementById('nxxune9mCfHvnxhZl3dO').value = text;
+    document.getElementById('nxxune9mCfHvnxhZl3dO').dispatchEvent(new Event("input", {
+      bubbles: true
+    }));
+    document.getElementById('schoolimagelogo1').src = text;
+    document.getElementById('logopreview1').style.display = "block";
+    document.getElementById('cleartext1').innerText = "Clear";
+  } catch (error) {
+    alert('Failed to read clipboard.');
+  }
+}
+
+// Clear Logo Function
+function clearFunction1() {
+  document.getElementById('logopreview1').style.display = "none";
+  document.getElementById('nxxune9mCfHvnxhZl3dO').value = "";
+  document.getElementById('nxxune9mCfHvnxhZl3dO').dispatchEvent(new Event("input", {
+    bubbles: true
+  }));
+  document.getElementById('schoolimagelogo1').src = "";
+  document.getElementById('cleartext1').innerText = "";
+}
+
+// Logo Background Transparency
+function MyFunc2() {
+  var x = document.getElementById("mediaimage");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    document.getElementById("mybutton2").innerHTML = "Is the background of my logo transparent? ↓"
+  } else {
+    x.style.display = "block";
+    document.getElementById("mybutton2").innerHTML = "Is the background of my logo transparent? ↑"
+  }
+}
+
+// Page Interval
+const myInterval = setInterval(intFunc, 100);
+
+function intFunc() {
+  var a = document.getElementById('pastebutton');
+  if (a === undefined || a === null) {} {
+    a.addEventListener('click', pastebtn);
+  }
+  var b = document.getElementById('clearbutton1');
+  if (b === undefined || b === null) {} {
+    b.addEventListener('click', clearFunction1);
+  }
+  var c = document.getElementById('mybutton2');
+  if (c === undefined || c === null) {} {
+    c.addEventListener('click', MyFunc2);
+  }
+  var x = document.getElementById("kNki5hB6i5ZvqlBEWjeT");
+  if (x === undefined || x === null) {} {
+    //encode
+    //user phone
+    //location id
+  }
+}

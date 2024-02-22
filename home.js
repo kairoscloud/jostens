ca5f571538414943eae26fab19c19d1ac9152017
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-22 09:14:55 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 var head= document.getElementsByTagName('head')[0];
 var script= document.createElement('script');
 var id = Math.random().toString(36).slice(2, 7);
-var src = "https://drewderose.github.io/jostenscustomcode/02_21_24_005.js?" + id;
+var src = "https://drewderose.github.io/jostenscustomcode/main.js?" + id;
 script.setAttribute("id", "jostenscustomcode_home");
 script.src= src;
 head.appendChild(script);

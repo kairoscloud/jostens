8a6df6ef39a09843dcbed8535abc1202bf79ba9e
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-23 15:35:11 -0500

    Update oauth-redirect-url.js

--- oauth-redirect-url.js
+++ oauth-redirect-url.js
@@ -1,6 +1,23 @@
-var url = window.location.href;
-var code = url.split("code=")[1];
-alert(code);
-document.getElementById("").innerText = code;
+// Load Function
+window.loadInt = setInterval(loadFunc, 100);
 
-navigator.clipboard.writeText(copyText.value);
+function loadFunc() {
+  var x = document.getElementById("button-ydAztZSX3U_btn");
+  if (x === undefined || x === null) {} else {
+    document.getElementById("button-ydAztZSX3U_btn").addEventListener('click', copyCode);
+    authcodeFunc();
+    clearInterval(loadInt);
+  }
+}
+
+// Get Authorization Code
+function authcodeFunc() {
+  window.url = window.location.href;
+  window.code = url.split("code=")[1];
+  document.getElementById("sub-heading-_9WWOhxpa1").children[0].children[0].children[0].innerText = code;
+}
+
+// Copy Code Function
+function copyCode() {
+  navigator.clipboard.writeText(code);
+}

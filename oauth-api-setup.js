f58bad90a9019c186fdd8d3ce52b465731739d62
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-23 15:01:10 -0500

    Update oauth-api-setup.js

--- oauth-api-setup.js
+++ oauth-api-setup.js
@@ -19,20 +19,5 @@ function editorFunc() {
   var sso = document.getElementById("FTHAL47JZmnqQZIwyvc9").value;
   var scope = document.getElementById("bwsVb6yruPE21WBFcCSM").value;
   var url = "https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://oauth.kairoscloud.io&client_id=" + id +"&scope=" + scope;
-  document.getElementById("CFX6jNxLm6WeiJaeUb3o").value = url;
-  document.getElementById("CFX6jNxLm6WeiJaeUb3o").dispatchEvent(new Event("input", {
-    bubbles: true
-  }));
-  if (id !== "" && scope !== "") {
-    document.getElementById("row-OQerrXbkGI").classList.remove("hide");
-  }
-}
-
-// Continue Function
-function continueFunc() {
-  document.getElementById("row-vmRU-jtNzU").style.display = "none";
-  document.getElementById("row-OQerrXbkGI").style.display = "none";
-  document.getElementById("row-_YL_7azO14").classList.remove("hide");
-  var url = document.getElementById("CFX6jNxLm6WeiJaeUb3o").value;
-  document.getElementById("iframe").src = url;
+  document.getElementById("sub-heading-WG6XAr3yzg").children[0].chilren[0].children[0].innerText = url;
 }

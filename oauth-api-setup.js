3088f9e5e35e8478071e2edda6a903a95fd7498a
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-23 15:03:54 -0500

    Update oauth-api-setup.js

--- oauth-api-setup.js
+++ oauth-api-setup.js
@@ -18,5 +18,9 @@ function editorFunc() {
   var sso = document.getElementById("FTHAL47JZmnqQZIwyvc9").value;
   var scope = document.getElementById("bwsVb6yruPE21WBFcCSM").value;
   var url = "https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://oauth.kairoscloud.io&client_id=" + id +"&scope=" + scope;
-  document.getElementById("sub-heading-WG6XAr3yzg").children[0].chilren[0].children[0].innerText = url;
+  document.getElementById("CFX6jNxLm6WeiJaeUb3o").value = url;
+  document.getElementById("CFX6jNxLm6WeiJaeUb3o").dispatchEvent(new Event("input", {
+    bubbles: true
+  }));
+  document.getElementById("sub-heading-WG6XAr3yzg").children[0].children[0].children[0].innerText = url;
 }

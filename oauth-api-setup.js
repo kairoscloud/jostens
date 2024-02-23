cd572e794028258a68326b343b25ef308df4d08b
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-23 12:02:20 -0500

    Update and rename oauth-api-setup to oauth-api-setup.js

---
+++ oauth-api-setup.js
@@ -0,0 +1,7 @@
+// Load Function
+const loadInt = setInterval(loadFunc, 100);
+
+function loadFunc() {
+  var x = document.getElementById("loaddiv");
+  if (x === undefined || x === null) {} else {
+    dd

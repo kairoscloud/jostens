1dbbc774b7681cb7068c004b6a05e32fac4dfd7a
drewderose <159578967+drewderose@users.noreply.github.com>
2024-02-27 13:06:13 -0500

    Update oauth-api-setup.js

--- oauth-api-setup.js
+++ oauth-api-setup.js
@@ -26,10 +26,7 @@ function fillformFunc() {
 
 // Editor Function
 function editorFunc() {
-  var name = document.getElementById("sLwJpaukHsCUNTMv7rFe").value;
   var id = document.getElementById("DMqsWSFgrZGG6HSv8ZGS").value;
-  var secret = document.getElementById("elUUxg52bvrZQbXeNkQi").value;
-  var sso = document.getElementById("FTHAL47JZmnqQZIwyvc9").value;
   var scope = document.getElementById("bwsVb6yruPE21WBFcCSM").value;
   var url = "https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https://oauth.kairoscloud.io/url-redirect&client_id=" + id +"&scope=" + scope;
   document.getElementById("CFX6jNxLm6WeiJaeUb3o").value = url;

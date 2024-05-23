44e9dc480e6ed1db3c7635687325fbedaf52f2fe
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-23 14:59:14 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -1,7 +1,41 @@
+// Jostens Custom Code
 var head= document.getElementsByTagName('head')[0];
 var script= document.createElement('script');
 var id = Math.random().toString(36).slice(2, 7);
 var src = "https://drewderose.github.io/jostenscustomcode/agency-content.js?" + id;
 script.setAttribute("id", "jostens-custom-code_agency-content");
+script.src= src;
+// Currently OFF, turn ON by including: head.appendChild(script);
+// GHL Customizer Code
+var head= document.getElementsByTagName('head')[0];
+var script= document.createElement('script');
+var id = Math.random().toString(36).slice(2, 7);
+var src = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" + id;
+script.setAttribute("id", "ghl-customizer-script");
+script.src= src;
+head.appendChild(script);
+// ThemeBuilder Code
+var head= document.getElementsByTagName('head')[0];
+var script= document.createElement('script');
+var id = Math.random().toString(36).slice(2, 7);
+var src = "https://builder.themarketerstoolkit.com/hlembed.js?" + id;
+script.setAttribute("id", "themebuilder-script");
+script.setAttribute("data-agency-id", "1x762kiz4");
+script.src= src;
+head.appendChild(script);
+// Fiverr Custom Code
+var head= document.getElementsByTagName('head')[0];
+var script= document.createElement('script');
+var id = Math.random().toString(36).slice(2, 7);
+var src = "https://fiverr.ghlbranding.com/drewderose/app.js?" + id;
+script.setAttribute("id", "fiverr-custom-code");
+script.src= src;
+head.appendChild(script);
+// Jacob Custom Code
+var head= document.getElementsByTagName('head')[0];
+var script= document.createElement('script');
+var id = Math.random().toString(36).slice(2, 7);
+var src = "https://drewderose.github.io/jostenscustomcode/refresh.js?" + id;
+script.setAttribute("id", "jbwx-code");
 script.src= src;
 head.appendChild(script);

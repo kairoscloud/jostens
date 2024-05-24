d145647a4ae10155aa6cccf11ab1f113c23c204b
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 15:34:36 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,5 +1,5 @@
-const myInterval = setInterval(testFunc1, 1000);
+window.onload = testFunc1;
 function testFunc1(){
-  var x = document.querySelector("#app > div > div > div > div > div.h-14.flex.items-center.px-4.text-lg.bg-elephant-500.text-white.w-full.justify-between.pb-2.pt-3");
+  var x = document.getElementById("close");
   console.log(x);
 }

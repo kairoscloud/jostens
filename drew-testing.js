55cd7b215fd950e3d9527c6ca8555c4d94344f2f
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 14:38:31 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,6 +1,4 @@
-console.log("script loaded!");
-setTimeout(deleteElems(), 10000);
-
-function deleteElems(){
-  console.log("Hello World!");
+setTimeout(testFunc1(), 5000);
+function testFunc1(){
+  console.log("Hello Drew!");
 }

89531daceffcf30d240ada64fd4f4fd0d4b15ba4
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-24 15:51:12 -0400

    Update media-library.js

--- media-library.js
+++ media-library.js
@@ -1,8 +1,8 @@
-alert("Code version 10.0");
+alert("Code version 11.0");
 const myTimer = setTimeout(testFunc2, 10000);
 function testFunc2(){
   alert("Timer done");
   //window.eval(document.querySelector("#app > div > div > div > div > div.mx-4.mt-5.hl-card").remove());
-  window.eval(document.querySelector("mx-4.mt-5.hl-card").remove());
+  window.eval("document.querySelector('mx-4.mt-5.hl-card').remove()");
   alert("Evaluation done");
 }

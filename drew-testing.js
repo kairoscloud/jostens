764edc308c5d3b6fec1658b5087aa9ee143f6b69
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 15:03:49 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,5 +1,5 @@
 const myTimer = setTimeout(testFunc1, 10000);
 function testFunc1(){
   console.log("Testing Function Loaded!");
-  document.querySelector("#close").style.display = "none";
+  document.getElementById("close").style.display = "none";
 }

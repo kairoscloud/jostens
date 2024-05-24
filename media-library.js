a3d693b16d71517d4e39a9b8758405f457939416
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-23 22:30:45 -0400

    Update media-library.js

--- media-library.js
+++ media-library.js
@@ -24,7 +24,7 @@ if (window.location.href === "https://app.kairoscloud.io/location/PqeI2v9lcicAtJ
     alert("Code updated 2");
         setTimeout(() => {
             alert("Selection attempted"),
-            (window.parent).document.querySelector("#app > div > div > div > div > div.mx-4.mt-5.hl-card").remove();
+            document.querySelector("#app > div > div > div > div > div.mx-4.mt-5.hl-card").remove();
         }, 20000); 
 
     window.onload = function() {

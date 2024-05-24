233d5505c758be2982eb0da2d10feada45f38674
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-24 15:09:08 -0400

    Update media-library.js

--- media-library.js
+++ media-library.js
@@ -1,31 +1,7 @@
-console.log("script loaded!");
-setTimeout(deleteElems(), 10000);
-
-function deleteElems(){
-
-// Delete the "media library" title.
-
-    // Find all h2 elements
-    var h2Elements = document.getElementsByTagName('h2');
-    
-    // Loop through all h2 elements to find the one with the text "Media Library"
-    for (var i = 0; i < h2Elements.length; i++) {
-        if (h2Elements[i].textContent.trim() === 'Media Library') {
-            // Remove the found element
-            h2Elements[i].parentNode.removeChild(h2Elements[i]);
-            break; // Exit the loop once the element is found and removed
-        }
-    }
-
-}
-
-
+const myTimer = setTimeout(testFunc2, 10000);
+function testFunc2(){
 if (window.location.href === "https://app.kairoscloud.io/location/PqeI2v9lcicAtJBI7mzs/medias") {
-    alert("Code version 7.0");
-        setTimeout(() => {
-            alert("Selection attempted"),
-            document.querySelector("#app > div > div > div > div > div.mx-4.mt-5.hl-card").remove();
-        }, 10000);
-
+  document.querySelector("#app > div > div > div > div > div.mx-4.mt-5.hl-card").remove();
+}
 }
 

d947576f05045c493ac693e6969cd9cec3fc9e47
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 15:16:16 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,5 +1,5 @@
 const myInterval = setInterval(testFunc1, 1000);
 function testFunc1(){
-  var x = document.getElementsByClassName("button").count();
+  var x = document.getElementsByClassName("button").length;
   console.log(x);
 }

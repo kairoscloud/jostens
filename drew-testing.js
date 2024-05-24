4c268af03897ca73bb36092c7703fa94a16faf34
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 14:52:30 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,4 +1,5 @@
-setTimeout(testFunc1(), 20000);
+setTimeout(testFunc1(), 50000);
 function testFunc1(){
+  console.log("Testing Function Loaded!");
   document.querySelector("#close").style.display = "none";
 }

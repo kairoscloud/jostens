f96a52358e66c0da6992318ddefcc4dc84dad2ef
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 15:19:19 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,5 +1,5 @@
 const myInterval = setInterval(testFunc1, 1000);
 function testFunc1(){
-  var x = document.getElementsByClassName("button").length;
+  var x = document.querySelector("#app > div > div > div > div > div.h-14.flex.items-center.px-4.text-lg.bg-elephant-500.text-white.w-full.justify-between.pb-2.pt-3");
   console.log(x);
 }

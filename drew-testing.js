9faa13ed2798407043b4e1196f9f79b280b3cc07
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 15:54:04 -0400

    Update drew-testing.js

--- drew-testing.js
+++ drew-testing.js
@@ -1,9 +1,4 @@
 setTimeout(func1, 5000);
 function func1(){
-  eval(testFunc1);
-  console.log("Eval Loaded!");
-}
-function testFunc1(){
-  document.getElementById("close").style.display = "none";
   alert("Testing");
 }

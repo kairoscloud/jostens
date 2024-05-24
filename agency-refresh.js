2b59850befbe72b26be5101f217acd5c7e24c460
drewderose <159578967+drewderose@users.noreply.github.com>
2024-05-24 14:42:38 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -12,7 +12,7 @@ var script2 = document.createElement('script');
 var id2 = Math.random().toString(36).slice(2, 7);
 var src2 = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" + id2;
 script2.setAttribute("id", "ghl-customizer-script");
-script2.src= sr2c;
+script2.src= src2;
 head2.appendChild(script2);
 // ThemeBuilder Code
 var head3 = document.getElementsByTagName('head')[0];

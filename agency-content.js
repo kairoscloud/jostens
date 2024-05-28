2132666151dd77d81049829b999cd30424b36c5e
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-28 09:46:27 -0400

    Update agency-content.js

--- agency-content.js
+++ agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 0.6");
+alert("Code version 0.7");
 const JostensTimer = setTimeout(JostensFunc, 3500);
 
 function JostensFunc() {
@@ -84,7 +84,7 @@ function ContactsFunc() {
       document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12) > button").remove();
       document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(13) > button").remove();
       document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(15) > button").remove();
-  }, 2000);
+  }, 4000);
 
    }
 }

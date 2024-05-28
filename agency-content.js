12e4abb41f49fec85d688b457f0793813853daae
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-28 10:23:51 -0400

    Update agency-content.js

--- agency-content.js
+++ agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 1.7");
+alert("Code version 1.8");
 const JostensTimer = setTimeout(JostensFunc, 3500);
 
 function JostensFunc() {
@@ -81,9 +81,10 @@ function ContactsFunc() {
     document.querySelector("#newbtn1").remove(); // remove the buttons added by Drew
 
     setTimeout(() => {
-      //document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
-      //document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
-      //document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
   }, 2000);
 
    }

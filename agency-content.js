9c8c7ff22f64d8db201b00673a5e89580a8f9ce0
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-28 10:16:29 -0400

    Update agency-content.js

--- agency-content.js
+++ agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 1.5");
+alert("Code version 1.6");
 const JostensTimer = setTimeout(JostensFunc, 3500);
 
 function JostensFunc() {
@@ -81,9 +81,9 @@ function ContactsFunc() {
     document.querySelector("#newbtn1").remove(); // remove the buttons added by Drew
 
     setTimeout(() => {
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(13)").remove();
       document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
-      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
-      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+      document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(11)").remove();
   }, 2000);
 
    }

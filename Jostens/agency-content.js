880198a981402cbbe34ae20285524fdcb5152d24
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-30 16:51:58 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -99,7 +99,7 @@ function ContactsFunc() {
         try {
         document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
         } catch (error) {}
-    }, 3000);
+    }, 3500);
 
    }
 }

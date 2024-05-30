5b3dc6a68fd52f363333bb62d74fbe6999138f68
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-05-30 16:41:48 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,5 +1,5 @@
-alert("Code version 2.9");
-const JostensTimer = setTimeout(JostensFunc, 2000);
+alert("Code version 3.1");
+const JostensTimer = setTimeout(JostensFunc, 3500);
 
 function JostensFunc() {
   var planid = document.getElementsByClassName("jostensdem")[0];

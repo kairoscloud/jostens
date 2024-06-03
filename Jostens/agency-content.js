e9757ff7d34fda6aafa51ae79b6542ca706aa373
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 11:33:27 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 2.6");
+alert("Code version 2.7");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
@@ -6,7 +6,32 @@ function testIfLoaded() {
     console.log("testing!");
     var lastElement = document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(17)");
     if (lastElement) {
-        alert("Loaded");
+        console.log("Loaded");
+        deleteElems();
         clearInterval(checkInterval);
     }
+}
+
+function deleteElems() {
+    try {
+        document.querySelector("#Copy\\ School\\ Settings").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#Paste\\ School\\ Settings").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#Add\\ school\\ Contact").remove(); 
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
 }

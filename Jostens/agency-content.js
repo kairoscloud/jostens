f36daf6d818880cb8a5b61591111dca0ba382df5
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 10:58:30 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,25 @@
-alert("Code version 2.1");
+alert("Code version 2.2");
 window.addEventListener('routeChangeEvent', function(e) {
     alert("Window loaded");
+    try {
+        document.querySelector("#Copy\\ School\\ Settings").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#Paste\\ School\\ Settings").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#Add\\ school\\ Contact").remove(); 
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
+    try {
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
+    } catch (error) {}
 });

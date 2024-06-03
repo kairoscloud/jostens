685ef9a3d4fa63a8bd56d6d2801ef3ca40361896
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 14:49:05 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 2.19");
+alert("Code version 2.20");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
@@ -13,7 +13,7 @@ function testIfLoaded() {
     }
 }
 
-function deleteElems() {
+function deleteElems() { // you can probably guess what this does
     document.querySelector("#Copy\\ School\\ Settings").remove();
     document.querySelector("#Paste\\ School\\ Settings").remove();
     document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(12)").remove();
@@ -29,6 +29,10 @@ function checkAndHideElement() {
         console.log("Found!");
         element.style.display = 'none';
         (Array.from(document.querySelectorAll('*')).find(el => el.textContent.trim() === 'Action*')).remove();
-        clearInterval(tagCheckInterval);
+    }
+
+    let element2 = document.querySelector('div.modal-body[data-v-4a572634]');
+    if(element2){
+        element2.remove();
     }
 }

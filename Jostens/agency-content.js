1d9bfda401c8403e553e2c5c47a85cbd0b2086a3
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 11:53:28 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 2.9");
+alert("Code version 2.10");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
@@ -23,8 +23,12 @@ function deleteElems() {
 }
 
 function listenForTagClick() {
-    (document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(6) > button")).addEventListener('click', checkInterval = setInterval(checkAndHideElement, 50));
-    (document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(7) > button")).addEventListener('click', checkInterval = setInterval(checkAndHideElement, 50));
+    (document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(6) > button")).addEventListener('click', checkForAction());
+    (document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(7) > button")).addEventListener('click', checkForAction());
+}
+
+function checkForAction() {
+    checkInterval = setInterval(checkAndHideElement, 50);
 }
 
 function checkAndHideElement() {

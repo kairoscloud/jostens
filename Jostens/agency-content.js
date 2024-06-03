96eaa9df80564cddf3259f3a08150a3ab82b4c40
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 12:11:24 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,15 +1,15 @@
-alert("Code version 2.11");
+alert("Code version 2.12");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
 function testIfLoaded() {
-    console.log("testing!");
     var lastElement = document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(17)");
     if (lastElement) {
         console.log("Loaded");
         clearInterval(checkInterval);
         deleteElems();
         listenForTagClick();
+        listenforEmailSMSClick();
     }
 }
 
@@ -32,10 +32,31 @@ function checkForAction() {
 }
 
 function checkAndHideElement() {
+    console.log("listening for action!");
     let element = document.getElementById('action');
     if (element != "" && element != null && element != undefined) {
         element.style.display = 'none';
         (Array.from(document.querySelectorAll('*')).find(el => el.textContent.trim() === 'Action*')).remove();
         clearInterval(tagCheckInterval);
+    }
+}
+
+function listenforEmailSMSClick(){
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(4)").addEventListener('click', checkforEmailSMSElement());
+    document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(5)").addEventListener('click', checkforEmailSMSElement());
+}
+
+function checkforEmailSMSElement(){
+    checkEmailSMSInterval = setInterval(checkAndHideEmailSMS, 50);
+}
+
+function checkAndHideEmailSMS() {
+    console.log("listening for SMS/Email action!");
+    let element = document.querySelector("#__BVID__330___BV_modal_content_").style.display;
+    if (element){
+        element.style.display = 'none';
+        setTimeout(function(){
+            element.style.display = 'block';
+        }, 2000);
     }
 }

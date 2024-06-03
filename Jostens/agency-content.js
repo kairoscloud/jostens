108cb07c7a8c793b9e826fdd9daa844466bc1044
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 14:04:31 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 2.15");
+alert("Code version 2.16");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
@@ -23,18 +23,18 @@ function deleteElems() {
 }
 
 function lookForElems(){ // this repeats every 100ms
-    let element = document.querySelector("#__BVID__330___BV_modal_content_");
-    if (element2){
+    let element = document.querySelector("#__BVID__330___BV_modal_content_"); // this is for the SMS/email dialogue box
+    if (element){
         element.style.display = 'none';
         setTimeout(function(){ // hide it, but unhide it after 2 seconds
             element.style.display = 'block';
         }, 2000);
     }
 
-    let element2 = document.getElementById('action');
+    let element2 = document.getElementById('action'); // this is for the "action" form
     if (element2) {
-        element2.style.display = 'none';
-        (Array.from(document.querySelectorAll('*')).find(el => el.textContent.trim() === 'Action*')).remove();
+        element2.style.display = 'none'; // make it invisible
+        (Array.from(document.querySelectorAll('*')).find(el => el.textContent.trim() === 'Action*')).remove(); // hide the label element above
     }
 
 }

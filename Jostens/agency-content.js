05d56a4884f8c4595fbf2880619636e5a9328ef2
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 11:18:33 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,10 +1,11 @@
-alert("Code version 2.2");
+alert("Code version 2.3");
 setInterval(testIfLoaded(), 100);
 
 
 function testIfLoaded() {
-    var x = document.getElementsByClassName("contactsloaded")[0];
-    if (x != undefined && x != null) {
-        alert("Contacts loaded");
+    console.log("testing!");
+    var lastElement = document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(17)");
+    if (lastElement) {
+        alert("Loaded");
     }
 }

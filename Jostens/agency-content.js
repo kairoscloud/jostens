d3fd22509a6d172c928c991e30bc2ef7b75fb083
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-03 11:28:07 -0400

    Update agency-content.js

--- Jostens/agency-content.js
+++ Jostens/agency-content.js
@@ -1,4 +1,4 @@
-alert("Code version 2.5");
+alert("Code version 2.6");
 checkInterval = setInterval(testIfLoaded, 100);
 
 
@@ -7,5 +7,6 @@ function testIfLoaded() {
     var lastElement = document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(17)");
     if (lastElement) {
         alert("Loaded");
+        clearInterval(checkInterval);
     }
 }

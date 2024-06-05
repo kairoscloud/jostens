e46dd5ba46d966e267c76cc5dcb4d3d84b5b944f
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-05 09:26:59 -0400

    Update directory.js

--- directory.js
+++ directory.js
@@ -7,7 +7,7 @@ window.id = Math.random().toString(36).slice(2, 7);
 window.addEventListener('routeChangeEvent', function(e) {
   console.log("Route Change Detected!");
   var url = window.location.href.split("/");
-  if (url.contains("contacts") === true) {
+  if (url.includes("contacts") === true) {
     var contacts = document.createElement('script');
     var src1 = "https://kairoscloud.github.io/jostens/contacts-page.js?" + id;
     contacts.setAttribute("id", "contacts-page-js");

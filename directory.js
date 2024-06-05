4245d753874f1dc7f531f4eb03e8788597786e0c
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-05 09:05:26 -0400

    Update directory.js

--- directory.js
+++ directory.js
@@ -1,1 +1,11 @@
 console.log("Jostens Directory Loaded!");
+window.addEventListener('routeChangeEvent', function(e) {
+  var url = window.location.href.split("/");
+  if (url.contains("contacts") === true) {
+    var contacts = document.createElement('script');
+    var src1 = "https://kairoscloud.github.io/jostens/contacts-page.js";
+    contacts.setAttribute("id", "contacts-page-js");
+    contacts.src= src1;
+    document.getElementsByTagName('head')[0].appendChild(contacts);
+  }
+})

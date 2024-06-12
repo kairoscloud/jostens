345945910ed37fede68a5bb43833ad286a087d0c
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-11 21:37:44 -0400

    Update directory.js

--- directory.js
+++ directory.js
@@ -108,8 +108,8 @@
       "#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(14)",
     );
     if (lastElement) {
-      addSchoolContactButton();
       DeleteElems();
+      addSchoolContactButton();
     }
   }
 

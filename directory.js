b224d387e1ac6869e75bb5f4f818bd68d843479b
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-12 18:34:46 -0400

    Update directory.js

--- directory.js
+++ directory.js
@@ -87,18 +87,18 @@
         '.form-row .form-group input[name="description"]',
       );
       actionForm.value = "dummyValue";
-      action.style.display = "none";
-      actionForm.style.display = "none";
+      // action.style.display = "none";
+      // actionForm.style.display = "none";
     }
 
     let actionOther = document.querySelector(".mt-2 .mt-1 input#action");
 
     if (actionOther) {
       actionOther.value = "dummyValue";
-      actionOther.style.display = "none";
-      Array.from(document.querySelectorAll("*")).find(
-        (el) => el.textContent.trim() === "Action*",
-      ).style.display = "none"; // hide the "action*" text above as well
+      // actionOther.style.display = "none";
+      // Array.from(document.querySelectorAll("*")).find(
+      //   (el) => el.textContent.trim() === "Action*",
+      // ).style.display = "none"; // hide the "action*" text above as well
     }
 
     // check for the last element in the toolbar (this happens when "all" is clicked, and the toolbar refreshes)

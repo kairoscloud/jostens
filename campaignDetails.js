2fec6d127312af8ce8818227002067ca3bd69943
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-26 11:46:51 -0400

    Update campaignDetails.js

--- campaignDetails.js
+++ campaignDetails.js
@@ -1,10 +1,11 @@
 let checkIfCampaignDetailsInterval = setInterval(() => {
   console.log("Listening for campaign details...");
-  if (
-    document.querySelector("#SubNavPaneHeader > div > div > div").innerText ==
-    "Campaign Details"
-  ) {
-    clearInterval(checkIfCampaignDetailsInterval);
+  if (document.querySelector("#SubNavPaneHeader > div > div > div")) {
+    if (
+      (document.querySelector("#SubNavPaneHeader > div > div > div").innerText =
+        "Campaign Details")
+    )
+      clearInterval(checkIfCampaignDetailsInterval);
     campaignDetails();
   }
 }, 3000);

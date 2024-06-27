dabf353757edcb6d8facb39e58154b357ef87818
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-26 21:19:12 -0400

    Update campaignDetails.js

--- campaignDetails.js
+++ campaignDetails.js
@@ -27,6 +27,9 @@ function campaignDetails() {
   document.querySelector("#ExampleSampleMessage1").innerHTML =
     "<div class='flex' style='cursor: pointer' onclick='sampleMessage1()'><span style='color: rgb(29, 41, 57); font-size: 14px; text-decoration: underline; font-weight: 500'>See example</span> <svg style='margin: 4px' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' aria-hidden='true' class='h-4 w-4 text-gray-800'><path stroke-linecap='round' stroke-linejoin='round' d='M6 9l6 6 6-6'></path></svg></div>";
 
+  document.querySelector("#ExampleSampleMessage2").innerHTML =
+    "<div class='flex' style='cursor: pointer' onclick='sampleMessage2()'><span style='color: rgb(29, 41, 57); font-size: 14px; text-decoration: underline; font-weight: 500'>See example</span> <svg style='margin: 4px' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' aria-hidden='true' class='h-4 w-4 text-gray-800'><path stroke-linecap='round' stroke-linejoin='round' d='M6 9l6 6 6-6'></path></svg></div>";
+
   // Popup insertable
 
   let popupInsertable = document.createElement("div");
@@ -43,6 +46,7 @@ function campaignDetails() {
     document.getElementById("clickableBackground").style.display = "none";
     document.getElementById("useCaseDescriptionPopup").style.display = "none";
     document.getElementById("sampleMessage1Popup").style.display = "none";
+    document.getElementById("sampleMessage2Popup").style.display = "none";
     // add more here
   };
   background.style = `
@@ -210,6 +214,99 @@ function campaignDetails() {
 `;
 
   popupInsertable.appendChild(sampleMessage1Popup);
+
+  // sample message 2
+
+  let sampleMessage2Popup = document.createElement("span");
+  sampleMessage2Popup.id = "sampleMessage2Popup";
+  sampleMessage2Popup.style = "display: none; z-index: 2002;";
+  sampleMessage2Popup.innerHTML = `<div
+      class="v-binder-follower-content"
+      v-placement="top-end"
+      style="
+          --v-target-width: 108px;
+          --v-target-height: 20px;
+          --v-offset-left: 0px;
+          --v-offset-top: 0px;
+          transform: translateX(970px) translateY(469px) translateX(-100%)
+              translateY(-100%);
+          --v-transform-origin: bottom right;
+          transform-origin: right bottom;
+      "
+  >
+      <div
+          class="n-popover n-popover-shared n-popover--show-header-or-footer n-popover-shared--show-arrow"
+          style="
+              width: 400px;
+              --n-box-shadow: 0 3px 6px -4px rgba(0, 0, 0, 0.12),
+                  0 6px 16px 0 rgba(0, 0, 0, 0.08),
+                  0 9px 28px 8px rgba(0, 0, 0, 0.05);
+              --n-bezier: cubic-bezier(0.4, 0, 0.2, 1);
+              --n-bezier-ease-in: cubic-bezier(0.4, 0, 1, 1);
+              --n-bezier-ease-out: cubic-bezier(0, 0, 0.2, 1);
+              --n-font-size: 14px;
+              --n-text-color: rgba(52, 64, 84, 1);
+              --n-color: #fff;
+              --n-divider-color: rgb(239, 239, 245);
+              --n-border-radius: 3px;
+              --n-arrow-height: 6px;
+              --n-arrow-offset: 10px;
+              --n-arrow-offset-vertical: 10px;
+              --n-padding: 8px 14px;
+              --n-space: 6px;
+              --n-space-arrow: 10px;
+          "
+      >
+          <div class="n-popover__header">
+              <span class="font-semibold text-gray-800"
+                  >Sample Message #2 Examples</span
+              >
+          </div>
+          <div class="n-popover__content">
+              <div class="flex flex-col gap-3">
+                  <div
+                      id="CopyTextBlock"
+                      class="flex items-start gap-3 rounded-lg border border-solid border-gray-300 bg-gray-50 p-3"
+                  >
+                      <div class="text-gray-600 hl-text-sm-regular">
+                          Hi Jane! This is [insert name] from Jostens. It’s time
+                          to start thinking about class rings for your Student!
+                          Please be watching your texts and email for more info in
+                          the days to come. Text or call me anytime with questions
+                          at [insert phone]. Reply STOP to unsubscribe.
+                      </div>
+                      <div
+                          class="custom-hidden items-center justify-start group-hover:flex"
+                          onclick="copyText('Hi Jane! This is [insert name] from Jostens. It’s time to start thinking about class rings for your Student! Please be watching your texts and email for more info in the days to come. Text or call me anytime with questions at [insert phone]. Reply STOP to unsubscribe.')"
+                      >
+                          <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="2"
+                              stroke="currentColor"
+                              aria-hidden="true"
+                              class="h-4 w-4 cursor-pointer text-gray-700 outline-0 hover:text-primary-600"
+                          >
+                              <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M16 16v2.8c0 1.12 0 1.68-.218 2.108a2 2 0 01-.874.874C14.48 22 13.92 22 12.8 22H5.2c-1.12 0-1.68 0-2.108-.218a2 2 0 01-.874-.874C2 20.48 2 19.92 2 18.8v-7.6c0-1.12 0-1.68.218-2.108a2 2 0 01.874-.874C3.52 8 4.08 8 5.2 8H8m3.2 8h7.6c1.12 0 1.68 0 2.108-.218a2 2 0 00.874-.874C22 14.48 22 13.92 22 12.8V5.2c0-1.12 0-1.68-.218-2.108a2 2 0 00-.874-.874C20.48 2 19.92 2 18.8 2h-7.6c-1.12 0-1.68 0-2.108.218a2 2 0 00-.874.874C8 3.52 8 4.08 8 5.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 00.874.874C9.52 16 10.08 16 11.2 16z"
+                              ></path>
+                          </svg>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!---->
+          <div class="n-popover-arrow-wrapper">
+              <div class="n-popover-arrow"></div>
+          </div>
+      </div>
+  </div>
+`;
+
+  popupInsertable.appendChild(sampleMessage2Popup);
 }
 
 function useCaseDescription() {
@@ -237,12 +334,24 @@ function sampleMessage1() {
   document.getElementById("sampleMessage1Popup").style.display = "block";
 }
 
+function sampleMessage2() {
+  let esm = document.querySelector("#ExampleSampleMessage2");
+
+  let x = esm.getBoundingClientRect().x + esm.getBoundingClientRect().width;
+  let y = esm.getBoundingClientRect().y + esm.getBoundingClientRect().height;
+  document.getElementById("sampleMessage2Popup").children[0].style.transform =
+    "translateX(" + x + "px) translateY(" + y + "px) translateX(-100%)";
+
+  document.getElementById("clickableBackground").style.display = "block";
+  document.getElementById("sampleMessage2Popup").style.display = "block";
+}
+
 // this function copies the text parameter to the clipboard
 function copyText(text) {
   document.getElementById("clickableBackground").style.display = "none";
 
-  // add each of the popups here
   document.getElementById("useCaseDescriptionPopup").style.display = "none";
   document.getElementById("sampleMessage1Popup").style.display = "none";
+  document.getElementById("sampleMessage2Popup").style.display = "none";
   navigator.clipboard.writeText(text);
 }

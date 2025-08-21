02e22b974cbee62176239b0b0d93f7a5204c3514
jbwx <jacobwestra0@gmail.com>
2025-08-21 11:08:47 -0400

    test commit

--- contacts.js
+++ contacts.js
@@ -1,4 +1,4 @@
-let cScript_ver = 32;
+let cScript_ver = 33;
 // The Kairos Cloud contacts custom script
 // What does it do?
 //  - Autofills the search field with whatever query is passed through the URL
